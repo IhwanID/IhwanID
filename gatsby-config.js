607c37db52a8f6544fa454403fb94724a6abc4c4
Ihwan <ihwan@indosystem.com>
2019-02-27 20:38:10 +0700

    update name blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `My Blog`,
+    author: `Ihwan ID`,
+    description: `A Blog by Ihwan ID`,
+    siteUrl: `https://ihwan.id/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `ihwan_id`,
     },
   },
   plugins: [
