c605f65da8a2553d3a07899347a9fc7fe8c0b9e5
Ihwan <ihwan@indosystem.com>
2019-08-30 06:51:16 +0700

    update UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import coffee from '../images/coffee.svg'
+import youtube from '../images/youtube.svg'
 import me from '../../content/images/me.jpg'
 
 export default class UserInfo extends Component {
@@ -9,14 +9,12 @@ export default class UserInfo extends Component {
         <div className="container note-container">
           <div className="flex-author">
             <p>
-             I'm Ihwan, a mobile software developer specializing in Android and iOS . I make
-              open source coding projects and write free, quality articles and tutorials that help
-              thousands of people daily. No ads, no sponsored posts, no bullshit.
+             I'm Ihwan, a Mobile Software Developer Specializing in Android and iOS from Majalengka.
             </p>
-            <a href="https://ko-fi.com/ihwanid" className="donate-button" target="_blank">
-              Buy me a coffee 
+            <a href="https://youtube.com/ihwand" className="donate-button" target="_blank">
+              Support My Channel
               {' '}
-            <img src={coffee} className="coffee-icon" />
+            <img src={youtube} className="coffee-icon" />
             </a>
           </div>
           <div className="flex-avatar">
