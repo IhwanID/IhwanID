01ad4f75318400cc63860107d057e2daec4d576d
ihwan <mynameisihwan@gmail.com>
2020-02-04 15:46:02 +0700

    update description for SEO

--- config/site.js
+++ config/site.js
@@ -1,18 +1,17 @@
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'CodeWithIhwan', // Navigation and Site Title
-  titleAlt: 'Code With Ihwan', // Title for JSONLD
-  description: 'Code your mobile application with Ihwan',
-  url: 'https://codewith.ihwan.id', // Domain of your site. No trailing slash!
-  siteUrl: 'https://codewith.ihwan.id', // url + pathPrefix
-  siteLanguage: 'id', // Language Tag on <html> element
-  logo: 'static/logo/logo.png', // Used for SEO
+  pathPrefix: '/',
+  title: 'CodeWithIhwan',
+  titleAlt: 'Code With Ihwan',
+  description: 'Website tempat belajar mobile programming berbahasa Indonesia lengkap. membahas teknologi native untuk Android, iOS dan Hybrid dengan Flutter.',
+  url: 'https://codewith.ihwan.id',
+  siteUrl: 'https://codewith.ihwan.id',
+  siteLanguage: 'id',
+  logo: 'static/logo/logo.png',
   banner: 'static/logo/banner.png',
-  // JSONLD / Manifest
-  favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'IhwanID', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Ihwan', // Author for schemaORGJSONLD
+  favicon: 'static/logo/favicon.png',
+  shortName: 'IhwanID',
+  author: 'Ihwan',
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
-  twitter: '@ihwan_id', // Twitter Username
+  twitter: '@Ihwan_ID',
 };
