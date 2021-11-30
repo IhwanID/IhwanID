c36f2b858162f67ae33f8901860dcee2bf656a9c
ihwan <ihwan.id@icloud.com>
2021-11-30 20:48:09 +0700

    update publish

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.8.0"),
         .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin", from: "0.1.0")
     ],
     targets: [
